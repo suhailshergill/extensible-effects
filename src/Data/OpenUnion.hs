48ec01cf7737cfdcaf2057a6c18534139fde154a
Suhail Shergill <suhailshergill@gmail.com>
2014-11-28 02:02:01 -0500

    sss/open-union: don't export constructor for Union
    resolves #13

--- src/Data/OpenUnion.hs
+++ src/Data/OpenUnion.hs
@@ -14,20 +14,25 @@
 -- and <http://okmij.org/ftp/Haskell/extensible/OpenUnion2.hs>.
 -- Open unions (type-indexed co-products) for extensible effects.
 --
--- TODO: re-evaluate <https://github.com/bfops/extensible-effects/issues/13>
 -- TODO: see if we can do away with Typeable constraints, perhaps by
 -- incorporating ideas from <http://okmij.org/ftp/Haskell/extensible/TList.hs>
-module Data.OpenUnion( Union (..)
-                      , Member
-                      , SetMember
-                      , (:>)
-                      , inj
-                      , prj
-                      , prjForce
-                      , decomp
-                      , unsafeReUnion
-                      , weaken
-                      ) where
+module Data.OpenUnion(
+  -- * Classes
+  Member
+  -- ** Monad transformer related
+  , SetMember
+    -- * Type-indexed co-product
+    -- ** Datatypes
+  , Union
+  , (:>)
+    -- ** Functions
+  , inj
+  , prj
+  , prjForce
+  , decomp
+  , unsafeReUnion
+  , weaken
+  ) where
 
 import Control.Applicative ((<$>))
 import Data.Typeable
