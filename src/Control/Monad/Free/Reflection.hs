800fbb5cb50c1514927239978e1b56cbb3986143
Suhail Shergill <suhailshergill@gmail.com>
2014-12-12 01:27:33 -0500

    sss/free: add docs + clean types/constructors
    - TCQueue -> TCQ
    - 'FC f' replaced by 'Kleisli (Free f)'
    - FMExp -> FreeExp
    - FM -> Free
    - (>>>=) -> (^>>=) and has been made top-level
    - added docs to functions

--- src/Control/Monad/Free/Reflection.hs
+++ src/Control/Monad/Free/Reflection.hs
@@ -21,16 +21,23 @@ import Control.Monad
 import Data.TASequence
 import Data.TASequence.FastCatQueue (FastTCQueue)
 
-type TCQueue = FastTCQueue
-type FC f = Kleisli (Free f)
-type FMExp f a b = TCQueue (FC f) a b
+-- | Specific type-aligned sequence used to store chain of monadic binds.
+type TCQ = FastTCQueue
 
+-- | Type used to denote monadic chain of binds. As expected this builds on the
+-- Kleisli representation.
+type FreeExp f a b = TCQ (Kleisli (Free f)) a b
+
+-- | The abstract Free datatype. Original work available at
+-- <http://okmij.org/ftp/Haskell/AlgorithmsH1.html#reflection-without-remorse>.
 data Free f a =
-  forall x. FM (FreeView f x) (FMExp f x a)
+  forall x. Free (FreeView f x) (FreeExp f x a)
 
+-- | Inject a pure value into Free
 freePure :: a -> Free f a
 freePure = fromView . Pure
 
+-- | Inject an impure value into Free
 freeImpure :: f (Free f a) -> Free f a
 freeImpure = fromView . Impure
 
@@ -52,20 +59,30 @@ instance Functor f => Applicative (Free f) where
   (<*>) = ap
 instance Functor f => Monad (Free f) where
   return = freePure
-  (FM m r) >>= f = FM m (r >< tsingleton (Kleisli f))
+  mx >>= f = mx ^>>= tsingleton (Kleisli f)
 
-data FreeView f a = Pure a
-                  | Impure (f (Free f a))
+-- | The traditional 'view' of Free constructions
+data FreeView f a = Pure a -- ^ case embedding pure values
+                  | Impure (f (Free f a)) -- ^ case embedding impure values
+                                          -- nested in @f@. Traditionally this
+                                          -- is the @Control.Monad.Free.Free@
+                                          -- constructor, but that's confusing.
 
+-- | A way to get a 'Free' construction from the view by constructing an
+-- explicit expression with one element.
 fromView :: FreeView f a -> Free f a
-fromView x = FM x tempty
+fromView x = Free x tempty
 
+-- | A way to evaluate the 'Free' construction to its view (i.e., head normal
+-- form). This includes the logic to perform one level of monadic bind as needed
+-- from the 'FreeExp' representation.
 toView :: Functor f => Free f a -> FreeView f a
-toView (FM h t) = case h of
+toView (Free h t) = case h of
   Pure x -> case tviewl t of
     TAEmptyL -> Pure x
-    Kleisli hc :< tc -> toView (hc x >>>= tc)
-  Impure f -> Impure (fmap (>>>= t) f)
-  where
-    (>>>=) :: Free f a -> FMExp f a b -> Free f b
-    (FM x ys) >>>= r = FM x (ys >< r)
+    hc :< tc -> toView (runKleisli hc x ^>>= tc)
+  Impure f -> Impure (fmap (^>>= t) f)
+
+-- | The essence of monadic '>>=', i.e., append/concatenation (of sorts)
+(^>>=) :: Free f a -> FreeExp f a b -> Free f b
+(Free x ys) ^>>= r = Free x (ys >< r)
