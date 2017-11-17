0e4891da872db39bc7c1ad093cf23f7964806347
Suhail Shergill <suhailshergill@gmail.com>
2017-11-16 20:03:54 -0500

    freer: remove Control.Eff.Cut
    will bring back later; this has delayed the release of 'freer' for too
    long.

--- test/Test.hs
+++ test/Test.hs
@@ -34,7 +34,6 @@ import Control.Monad (liftM2, msum, guard, mzero, mplus)
 import Data.Monoid
 
 import Control.Eff.Example as Eg
---import Control.Eff.Cut
 import Control.Eff.Fresh
 import Control.Eff.Lift
 import Control.Eff.Operational as Op
@@ -975,36 +974,5 @@ case_Fresh_tfresh' = do
       trace $ "Fresh " ++ show n
       n <- fresh
       trace $ "Fresh " ++ show n
-
--- }}}
-
--- {{{ TODO: cut
-
--- -- The signature is inferred
--- tcut1 :: (Member Choose r, Member (Exc CutFalse) r) => Eff r Int
--- tcut1 = (return (1::Int) `mplus'` return 2) `mplus'`
---          ((cutfalse `mplus'` return 4) `mplus'`
---           return 5)
-
--- tcut1r = run . makeChoice $ call tcut1
--- -- [1,2]
-
--- tcut2 = return (1::Int) `mplus'`
---          call (return 2 `mplus'` (cutfalse `mplus'` return 3) `mplus'`
---                return 4)
---        `mplus'` return 5
-
--- -- Here we see nested call. It poses no problems...
--- tcut2r = run . makeChoice $ call tcut2
--- -- [1,2,5]
-
--- -- More nested calls
--- tcut3 = call tcut1 `mplus'` call (tcut2 `mplus'` cutfalse)
--- tcut3r = run . makeChoice $ call tcut3
--- -- [1,2,1,2,5]
-
--- tcut4 = call tcut1 `mplus'`  (tcut2 `mplus'` cutfalse)
--- tcut4r = run . makeChoice $ call tcut4
--- -- [1,2,1,2,5]
 
 -- }}}
