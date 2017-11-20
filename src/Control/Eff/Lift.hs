60850dd11c6141d22f66b474eb635be824b7d5d9
Suhail Shergill <suhailshergill@gmail.com>
2017-11-19 23:09:50 -0500

    freer: add catchDynE
    atching of dynamic and asynchronous exceptions in the presence of other
    effects -- the solution of the MonadCatchIO problem.

--- src/Control/Eff/Lift.hs
+++ src/Control/Eff/Lift.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE CPP #-}
+{-# LANGUAGE RankNTypes, ScopedTypeVariables #-}
 {-# LANGUAGE Safe #-}
 -- | Lifting primitive Monad types to effectful computations.
 -- We only allow a single Lifted Monad because Monads aren't commutative
@@ -8,9 +8,11 @@
 module Control.Eff.Lift ( Lift (..)
                        , lift
                        , runLift
+                       , catchDynE
                        ) where
 
 import Control.Eff
+import qualified Control.Exception as Exc
 import Data.OpenUnion
 
 -- ------------------------------------------------------------------------
@@ -28,3 +30,17 @@ runLift (Val x) = return x
 runLift (E u q) = case prj u of
                   Just (Lift m) -> m >>= runLift . qApp q
                   Nothing -> error "Impossible: Nothing cannot occur"
+
+-- | Catching of dynamic exceptions
+-- See the problem in
+-- http://okmij.org/ftp/Haskell/misc.html#catch-MonadIO
+catchDynE :: forall e a r.
+             (SetMember Lift (Lift IO) r, Exc.Exception e) =>
+             Eff r a -> (e -> Eff r a) -> Eff r a
+catchDynE m eh = interpose return h m
+ where
+   -- Polymorphic local binding: signature is needed
+   h :: Lift IO v -> Arr r v a -> Eff r a
+   h (Lift em) k = lift (Exc.try em) >>= \x -> case x of
+         Right x0 -> k x0
+         Left  e -> eh e
--- test/Control/Eff/Lift/Test.hs
+++ test/Control/Eff/Lift/Test.hs
@@ -1,13 +1,18 @@
-{-# LANGUAGE FlexibleContexts, ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts, ScopedTypeVariables, NoMonomorphismRestriction #-}
+{-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Control.Eff.Lift.Test (testGroups) where
 
 import Test.HUnit hiding (State)
 import Control.Eff
+import Control.Eff.Exception
 import Control.Eff.Lift
 import Control.Eff.Reader.Strict
+import Control.Eff.State.Strict
+import qualified Control.Exception as Exc
 import Data.OpenUnion
+import Data.Typeable
 import Utils
 
 import Test.Framework.TH
@@ -56,3 +61,161 @@ case_Lift_tMd' = do
       h' <- f h
       t' <- mapMdebug' f t
       return (h':t')
+
+-- tests from <http://okmij.org/ftp/Haskell/misc.html#catch-MonadIO>
+data MyException = MyException String deriving (Show, Typeable)
+instance Exc.Exception MyException
+
+exfn True = lift . Exc.throw $ (MyException "thrown")
+exfn False = return True
+
+testc m = catchDynE (m >>= return . show) (\ (MyException s) -> return s)
+
+case_catchDynE_test1 :: Assertion
+case_catchDynE_test1 = do
+  ((), actual) <- catchOutput test1
+  let expected = unlines [ "(\"thrown\",[\"begin\"])"
+                         , "(\"True\",[\"end\",\"begin\"])"]
+  assertEqual "catchDynE: test1: exception shouldn't drop Writer's state"
+    expected actual
+  where
+    -- In CatchMonadIO, the result of tf True is ("thrown",[]) --
+    -- that is, an exception will drop the Writer's state, even if that
+    -- exception is caught. Here, the state is preserved!
+    -- So, this is an advantage over MTL!
+    test1 = do runLift (tf True) >>= print; runLift (tf False) >>= print
+    tf x = runReader (runState (testc m) ([]::[String])) (x::Bool)
+    m = do
+      modify ("begin":)
+      x <- ask
+      r <- exfn x
+      modify ("end":)
+      return r
+
+-- Let us use an Error effect instead
+case_catchDynE_test1' :: Assertion
+case_catchDynE_test1' = do
+  ((), actual') <- catchOutput test1'
+  let expected' = unlines [ "(Left \"thrown\",[\"begin\"])"
+                         , "(Right \"True\",[\"end\",\"begin\"])"]
+  assertEqual "catchDynE: test1': Error shouldn't drop Writer's state"
+    expected' actual'
+  where
+    -- In CatchMonadIO, the result of tf True is ("thrown",[]) --
+    -- that is, an exception will drop the Writer's state, even if that
+    -- exception is caught. Here, the state is preserved!
+    -- So, this is an advantage over MTL!
+    test1' = do runLift (tf True) >>= print; runLift (tf False) >>= print
+    tf x = runReader (runState (runErrorStr (testc m)) ([]::[String])) (x::Bool)
+    m = do
+      modify ("begin":)
+      x <- ask
+      r <- exfn x
+      modify ("end":)
+      return r
+
+    runErrorStr = asEStr . runExc
+    asEStr :: m (Either String a) -> m (Either String a)
+    asEStr = id
+    exfn True = throwExc $ ("thrown")
+    exfn False = return True
+
+-- Now, the behavior of the dynamic Exception and Error effect is consistent.
+-- The state is preserved. Before it wasn't.
+case_catchDynE_test2 :: Assertion
+case_catchDynE_test2 = do
+  ((), actual) <- catchOutput test2
+  let expected = unlines [ "(Left \"thrown\",[\"begin\"])"
+                         , "(Right \"True\",[\"end\",\"begin\"])"]
+  assertEqual "catchDynE: test2: Error shouldn't drop Writer's state"
+    expected actual
+  where
+    test2 = do runLift (tf True) >>= print; runLift (tf False) >>= print
+    tf x = runReader (runState (runErrorStr (testc m)) ([]::[String])) (x::Bool)
+    runErrorStr = asEStr . runExc
+    asEStr :: m (Either String a) -> m (Either String a)
+    asEStr = id
+    m = do
+      modify ("begin":)
+      x <- ask
+      r <- exfn x `catchDynE` (\ (MyException s) -> throwExc s)
+      modify ("end":)
+      return r
+
+-- Full recovery
+case_catchDynE_test2' :: Assertion
+case_catchDynE_test2' = do
+  ((), actual) <- catchOutput test2'
+  let expected = unlines [ "(Right \"False\",[\"end\",\"begin\"])"
+                         , "(Right \"True\",[\"end\",\"begin\"])"]
+  assertEqual "catchDynE: test2': Fully recover from errors"
+    expected actual
+  where
+    test2' = do runLift (tf True) >>= print; runLift (tf False) >>= print
+    tf x = runReader (runState (runErrorStr (testc m)) ([]::[String])) (x::Bool)
+    runErrorStr = asEStr . runExc
+    asEStr :: m (Either String a) -> m (Either String a)
+    asEStr = id
+    m = do
+      modify ("begin":)
+      x <- ask
+      r <- exfn x `catchDynE` (\ (MyException s) -> return False)
+      modify ("end":)
+      return r
+
+-- Throwing within a handler
+case_catchDynE_test3 :: Assertion
+case_catchDynE_test3 = do
+  ((), actual) <- catchOutput test3
+  let expected = unlines [ "(Right \"rethrow:thrown\",[\"begin\"])"
+                         , "(Right \"True\",[\"end\",\"begin\"])"]
+  assertEqual "catchDynE: test3: Throwing within a handler"
+    expected actual
+  where
+    test3 = do runLift (tf True) >>= print; runLift (tf False) >>= print
+    tf x = runReader (runState (runErrorStr (testc m)) ([]::[String])) (x::Bool)
+    runErrorStr = asEStr . runExc
+    asEStr :: m (Either String a) -> m (Either String a)
+    asEStr = id
+    m = do
+      modify ("begin":)
+      x <- ask
+      r <- exfn x `catchDynE` (\ (MyException s) ->
+                                 lift . Exc.throw . MyException $
+                                 ("rethrow:" ++ s))
+      modify ("end":)
+      return r
+
+-- Implement the transactional behavior: when the exception is raised,
+-- the state is rolled back to what it existed at the entrance to
+-- the catch block.
+-- This is the ``scoping behavior'' of `Handlers in action'
+case_catchDynE_tran :: Assertion
+case_catchDynE_tran = do
+  ((), actual) <- catchOutput tran
+  let expected = unlines ["(\"thrown\",[\"init\"])"
+                         ,"(\"True\",[\"end\",\"begin\",\"init\"])"]
+  assertEqual "catchDynE: tran: Transactional behaviour"
+    expected actual
+  where
+    tran = do runLift (tf True) >>= print; runLift (tf False) >>= print
+    tf x = runReader (runState m1 ([]::[String])) (x::Bool)
+    m1 = do
+      modify ("init":)
+      testc (transactionState (ProxyState :: ProxyState [String]) m)
+    m = do
+      modify ("begin":)
+      x <- ask
+      r <- exfn x
+      modify ("end":)
+      return r
+{- -- without transaction
+("thrown",["begin","init"])
+("True",["end","begin","init"])
+-}
+
+-- With transaction
+{-
+("thrown",["init"])
+("True",["end","begin","init"])
+-}
