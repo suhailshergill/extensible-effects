bcd406c62c787851a2a14d90e14eca73dabcdbd4
Suhail Shergill <suhailshergill@gmail.com>
2018-01-02 07:01:58 -0500

    version: 2.3.0.1 => 2.4.0.0

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@
 }:
 mkDerivation {
   pname = "extensible-effects";
-  version = "2.3.0.1";
+  version = "2.4.0.0";
   src = ./.;
   libraryHaskellDepends = [ base transformers transformers-base ];
   testHaskellDepends = [
