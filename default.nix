5cda598d4809cb26816c0c56df4739f6256cfdb1
Suhail Shergill <suhailshergill@gmail.com>
2018-01-17 05:39:49 -0500

    version: 2.4.0.0 => 2.5.0.0

--- default.nix
+++ default.nix
@@ -1,14 +1,19 @@
-{ mkDerivation, base, HUnit, QuickCheck, silently, stdenv
-, test-framework, test-framework-hunit, test-framework-quickcheck2
-, test-framework-th, transformers, transformers-base
+{ mkDerivation, base, criterion, HUnit, mtl, QuickCheck, silently
+, stdenv, test-framework, test-framework-hunit
+, test-framework-quickcheck2, test-framework-th, transformers
+, transformers-base
 }:
 mkDerivation {
   pname = "extensible-effects";
-  version = "2.4.0.0";
+  version = "2.5.0.0";
   src = ./.;
   libraryHaskellDepends = [ base transformers transformers-base ];
   testHaskellDepends = [
     base HUnit QuickCheck silently test-framework test-framework-hunit
+    test-framework-quickcheck2 test-framework-th
+  ];
+  benchmarkHaskellDepends = [
+    base criterion HUnit mtl test-framework test-framework-hunit
     test-framework-quickcheck2 test-framework-th
   ];
   homepage = "https://github.com/suhailshergill/extensible-effects";
