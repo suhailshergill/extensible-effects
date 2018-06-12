a21262102f354de884eedab7e4e46d899c5b3dac
power-fungus <samuel.pilz@posteo.net>
2018-06-12 16:12:03 +0200

    retract other changes from pull-request

--- src/Control/Eff.hs
+++ src/Control/Eff.hs
@@ -1,14 +1,6 @@
-{-# OPTIONS_GHC -Wno-duplicate-exports #-}
-
-module Control.Eff
-  ( -- * Effect base-type
-    Internal.Eff
-    -- * Effect list
-  , OpenUnion.Member
-    -- Rest (not included in documentation)
-  , module Internal
-  , module OpenUnion
-  ) where
+module Control.Eff ( module Internal
+                   , module OpenUnion
+                   ) where
 
 import Control.Eff.Internal as Internal hiding (Lift(..), lift, runLift)
 import Data.OpenUnion as OpenUnion
--- src/Control/Eff/ImplementYourOwnEffect.hs
+++
@@ -1,1 +0,0 @@
-module Control.Eff.ImplementYourOwnEffect where
