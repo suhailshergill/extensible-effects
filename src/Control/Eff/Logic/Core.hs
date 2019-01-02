fbdab40eb5c61c692a3cb9f70eb64dc73ba1d844
Suhail Shergill <suhailshergill@gmail.com>
2019-01-02 14:00:39 -0500

    docs: eff/logic: fix formatting

--- src/Control/Eff/Logic/Core.hs
+++ src/Control/Eff/Logic/Core.hs
@@ -2,7 +2,9 @@
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE LambdaCase #-}
 
--- | Logic primitives. See LogicT paper for details.
+-- | Logic primitives. See @LogicT@ paper for details.
+--
+-- * [@LogicT@] [LogicT - backtracking monad transformer with fair operations and pruning](http://okmij.org/ftp/Computation/monads.html#LogicT)
 module Control.Eff.Logic.Core where
 
 import Control.Monad
@@ -16,8 +18,8 @@ import Data.Function (fix)
 class MSplit m where
   -- | The laws for 'msplit' are:
   --
-  -- 1] msplit mzero == return Nothing
-  -- 2] msplit (return a `mplus` m) == return (Just(a, m))
+  -- > msplit mzero                = return Nothing
+  -- > msplit (return a `mplus` m) = return (Just(a, m))
   msplit :: m a -> m (Maybe (a, m a))
 
 -- | Embed a pure value into MSplit
@@ -28,8 +30,9 @@ withMSplit a rest = return (Just (a, rest))
 -- effects (instead of by way of a distinct MSplit handler
 
 -- | Laws for 'reflect':
--- > msplit (lift m >> mzero) >>= reflect == lift m >> mzero
--- > msplit (lift m `mplus` ma) >>= reflect == lift m `mplus` (msplit ma >>= reflect)
+--
+-- > msplit (lift m >> mzero)   >>= reflect = lift m >> mzero
+-- > msplit (lift m `mplus` ma) >>= reflect = lift m `mplus` (msplit ma >>= reflect)
 {-# INLINE reflect #-}
 reflect :: MonadPlus m => Maybe (a, m a) -> m a
 reflect Nothing      = mzero
@@ -38,17 +41,18 @@ reflect (Just (a,m)) = return a `mplus` m
 -- Other committed choice primitives can be implemented in terms of msplit
 -- The following implementations are directly from the LogicT paper
 
--- | Soft-cut: non-deterministic if-then-else, aka Prolog's *->
+-- | Soft-cut: non-deterministic if-then-else, aka Prolog's @*->@
 -- Declaratively,
---    ifte t th el = (t >>= th) `mplus` ((not t) >> el)
--- However, t is evaluated only once. In other words, ifte t th el
--- is equivalent to t >>= th if t has at least one solution.
--- If t fails, ifte t th el is the same as el.
 --
--- Laws:
--- 1] ifte (return a) th el == th a
--- 2] ifte mzero th el == el
--- 3] ifte (return a `mplus` m) th el == th a `mplus` (m >>= th)
+-- >  ifte t th el = (t >>= th) `mplus` ((not t) >> el)
+--
+-- However, @t@ is evaluated only once. In other words, @ifte t th el@
+-- is equivalent to @t >>= th@ if @t@ has at least one solution.
+-- If @t@ fails, @ifte t th el@ is the same as @el@. Laws:
+--
+-- > ifte (return a) th el           = th a
+-- > ifte mzero th el                = el
+-- > ifte (return a `mplus` m) th el = th a `mplus` (m >>= th)
 ifte :: (MonadPlus m, MSplit m)
      => m t -> (t -> m b) -> m b -> m b
 ifte t th el = msplit t >>= check
@@ -69,11 +73,12 @@ gnot m = ifte (once m) (const mzero) (return ())
 -- | Fair (i.e., avoids starvation) disjunction. It obeys the
 -- following laws:
 --
--- 1] interleave mzero m == m
--- 2] interleave (return a `mplus` m1) m2 == return a `mplus` (interleave m2 m1)
+-- > interleave mzero m                  = m
+-- > interleave (return a `mplus` m1) m2 = return a `mplus` (interleave m2 m1)
 --
 -- corollary:
---   interleave m mzero == m
+--
+-- > interleave m mzero = m
 interleave :: (MSplit m, MonadPlus m) => m b -> m b -> m b
 interleave sg1 sg2 =
   do r <- msplit sg1
@@ -85,8 +90,8 @@ interleave sg1 sg2 =
 -- | Fair (i.e., avoids starvation) conjunction. It obeys the
 -- following laws:
 --
--- 1] mzero >>- k == mzero
--- 2] (return a `mplus` m) >>- k == interleave (k a) (m >>- k)
+-- > mzero                >>- k = mzero
+-- > (return a `mplus` m) >>- k = interleave (k a) (m >>- k)
 (>>-) :: (MonadPlus m, MSplit m) => m a -> (a -> m b) -> m b
 sg >>- g =
   do r <- msplit sg
@@ -101,9 +106,13 @@ sols m = (msplit m) >>= (fix step) [] where
   step _ jq Nothing          = return jq
   step next jq (Just(a, ma)) = (msplit ma) >>= next (a:jq)
 
--- | Non-determinism with control (cut)
--- For the explanation of cut, see Section 5 of Hinze ICFP 2000 paper.
--- Hinze suggests expressing cut in terms of cutfalse:
+-- | Non-determinism with control (@cut@).
+--
+-- For the explanation of cut, see Section 5 of Hinze ICFP 2000 paper:
+--
+-- * [@Backtr@] [Deriving Backtracking Monad Transformers](https://dl.acm.org/citation.cfm?id=351240.351258)
+--
+-- Hinze suggests expressing @cut@ in terms of @cutfalse@:
 --
 -- > = return () `mplus` cutfalse
 -- > where
@@ -134,12 +143,14 @@ sols m = (msplit m) >>= (fix step) [] where
 -- monad transformer with cut: no axiom specifying the interaction of
 -- call with bind; no way to simplify nested invocations of call.
 class Call r where
-  -- | Every instance should ensure that the following laws hold:
-  -- >  cutfalse | m    = cutfalse                  (F2)
-  -- >  call false = false                          (C1)
-  -- >  call (return a | m) = return a | call m     (C2)
-  -- >  call (m | cutfalse) = call m                (C3)
-  -- >  call (lift m >>= k) = lift m >>= (call . k) (C4)
+  -- | Mapping @Backtr@ interface to 'MonadPlus' and using exceptions for
+  -- @cutfalse@, every instance should ensure that the following laws hold:
+  --
+  -- >  cutfalse `mplus` m        = cutfalse                --(F2)
+  -- >  call mzero                = mzero                   --(C1)
+  -- >  call (return a `mplus` m) = return a `mplus` call m --(C2)
+  -- >  call (m `mplus` cutfalse) = call m                  --(C3)
+  -- >  call (lift m >>= k)       = lift m >>= (call . k)   --(C4)
   call :: MonadPlus (Eff r) => Eff (Exc CutFalse : r) a -> Eff r a
 
 data CutFalse = CutFalse
